b858d6fdece536a11cb1e8ab9bbb79660847c112
hassaanali124 <hassaanaliips@gmail.com>
2019-09-04 11:55:24 +0530

    programming

---
+++ javaScript/fact.js
@@ -0,0 +1,8 @@
+function ali(number){
+    var r=1;
+    for (i=number;i>=1;i--){
+        r=r*i;
+    }
+return r;   
+}
+console.log(ali(5));
---
+++ javaScript/hassaan.js
@@ -0,0 +1,4 @@
+var a=["hassaanali","jack"];
+for (var i=0;i<2;i++){
+    console.log(a[i]);
+}
