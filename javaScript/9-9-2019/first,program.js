901eb7e56eeb1b47801e7d0d67c1f9cad616d7a5
hassaanali124 <hassaanaliips@gmail.com>
2019-09-16 16:25:12 +0530

    fc

---
+++ javaScript/9-9-2019/first,program.js
@@ -0,0 +1,1 @@
+let total = 0, count = 1; while (count <= 10) { total += count; count += 1; } console.log(total); // → 55
--- javaScript/9-9-2019/function.js
+++ javaScript/9-9-2019/function.js
@@ -1,6 +1,6 @@
 function ali(num){
-    var result =1;
-    for (var i=num;i>=1;i--){
+    var result =2;
+    for (var i=num;i<=10;i--){
         result=result*i;
     } 
     return result;
