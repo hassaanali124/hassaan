26e63117a7b39a5ec9073fd457275ded2eddec00
hassaanali124 <hassaanaliips@gmail.com>
2019-09-19 13:41:52 +0530

    tc

---
+++ javaScript/tutorial/5,table.js
@@ -0,0 +1,3 @@
+for(var x=1;x<=10;x++){
+    console.log(8*x);
+}
---
+++ javaScript/tutorial/day.js
@@ -0,0 +1,26 @@
+var day=55;
+switch(day){
+case 1: 
+console.log("SUNDAY");
+break;
+case 2: 
+console.log("MONDAY");
+break;
+case 3: 
+console.log("TUESDAY");
+break;
+case 4: 
+console.log("WEDNESDAY");
+break;
+case 5: 
+console.log("THURSDAY");
+break;
+case 6: 
+console.log("FRIDAY");
+break;
+case 7: 
+console.log("SATURDAY");
+break;
+default:
+        console.log("NO DAY FOUND");
+}
