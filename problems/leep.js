ebb9356266371c2fb22df34efaaf631224acf7b1
hassaanali124 <hassaanaliips@gmail.com>
2019-09-20 16:47:17 +0530

    1

---
+++ problems/leep.js
@@ -0,0 +1,21 @@
+// var a = 0 ;
+// if(a>1990&& a<2019){
+//     if(a%4==0){
+//         console.log("it is a leep year");
+//     }
+//     else{
+//         console.log("it is not a leep year");
+//     }
+// }
+
+function leap (a) {
+    if(a>1990&& a<2019){
+        if(a%4==0){
+            console.log("it is a leep year");
+        }
+        else{
+            console.log("it is not a leep year");
+        }
+    }
+}
+leap(2006)
